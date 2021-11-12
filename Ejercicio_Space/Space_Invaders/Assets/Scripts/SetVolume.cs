86bd247c36a9aec6df22ffe70a52708f1a577cbb
Sergitotf <sergio_tf_vfx@hotmail.com>
2021-11-12 12:41:38 +0000

    12/11 12:41
    Entrega Final dia 12/11. Falla PlayerPref, Cancelar

--- Ejercicio_Space/Space_Invaders/Assets/Scripts/SetVolume.cs
+++ Ejercicio_Space/Space_Invaders/Assets/Scripts/SetVolume.cs
@@ -3,30 +3,45 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.Audio;
+using UnityEngine.Localization.Settings;
 
 public class SetVolume : MonoBehaviour
 {
     
     public AudioSource globalMusic;
     public Slider musicaSlider;
+    public LocalizationSettings idiomaSettings;
+    public int index;
 
     void Start()
     {
-        PlayerPrefs.SetFloat("MusicVolume", globalMusic.volume);
+        musicaSlider.value = PlayerPrefs.GetFloat("MusicVolume");
+        globalMusic.volume = musicaSlider.value;
+       /* PlayerPrefs.SetFloat("MusicVolume", globalMusic.volume);
         musicaSlider.value = globalMusic.volume;
+        PlayerPrefs.SetInt("index", index);*/
     }
     
     public void AcceptMusicVolume()
     {
         globalMusic.volume = musicaSlider.value;
+        PlayerPrefs.SetFloat("MusicVolume", musicaSlider.value);
+        /*
         PlayerPrefs.SetFloat("MusicVolume", globalMusic.volume);
+        PlayerPrefs.SetInt("index", index);
+        idiomaSettings.SetSelectedLocale(LocalizationSettings.AvailableLocales.Locales[PlayerPrefs.GetInt("Index")]); */
 
     }
     public void CancelMusicVolume()
     {
         globalMusic.volume = PlayerPrefs.GetFloat("MusicVolume");
         musicaSlider.value = PlayerPrefs.GetFloat("MusicVolume");
+        idiomaSettings.SetSelectedLocale(LocalizationSettings.AvailableLocales.Locales[PlayerPrefs.GetInt("Index")]);
 
+    }
+    public void CambioIndex(int indice)
+    {
+        index = indice;
     }
 }
 
