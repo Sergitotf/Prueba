21ab767990c55984b22d012f674936910d0f9841
Sergitotf <sergio_tf_vfx@hotmail.com>
2022-02-11 11:01:04 +0000

    11/02
    avance dia de entrega, principio animaciones enemigos

--- Ruby/Ruby2D/Assets/Scripts/RubyController.cs
+++ Ruby/Ruby2D/Assets/Scripts/RubyController.cs
@@ -8,14 +8,20 @@ public class RubyController : MonoBehaviour
     public int maxHealth = 5;
     public int health { get { return currentHealth; } }
     int currentHealth;
+    public float timeInvincible = 2.0f;
+    bool isInvincible;
+    float invincibleTimer;
     Rigidbody2D rigidbody2d;
 
     // Start is called before the first frame update
     void Start()
     {
+        QualitySettings.vSyncCount = 0;
+        Application.targetFrameRate = 10;
+
         rigidbody2d = GetComponent<Rigidbody2D>();
         currentHealth = maxHealth;
-        currentHealth = 1;
+        
     }
 
     // Update is called once per frame
@@ -29,9 +35,25 @@ public class RubyController : MonoBehaviour
         position.y = position.y + speed * vertical * Time.deltaTime;
 
         rigidbody2d.MovePosition(position);
+
+        if (isInvincible)
+        {
+            invincibleTimer -= Time.deltaTime;
+            if (invincibleTimer < 0)
+                isInvincible = false;
+        }
     }
+
     public void ChangeHealth(int amount)
     {
+        if (amount < 0)
+        {
+            if (isInvincible)
+                return;
+
+            isInvincible = true;
+            invincibleTimer = timeInvincible;
+        }
         currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
         Debug.Log(currentHealth + "/" + maxHealth);
     }
