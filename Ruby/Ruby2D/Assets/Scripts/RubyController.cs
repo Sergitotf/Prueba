d05ad8724d82a26ab547bae3a5e10b89c908b7ac
Sergitotf <sergio_tf_vfx@hotmail.com>
2022-02-17 13:23:39 +0000

    17/02 ultima clase
    reparar animaciones y colisiones del enemigo

--- Ruby/Ruby2D/Assets/Scripts/RubyController.cs
+++ Ruby/Ruby2D/Assets/Scripts/RubyController.cs
@@ -78,7 +78,8 @@ public class RubyController : MonoBehaviour
             animator.SetTrigger("Hit");
         }
         currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
-        Debug.Log(currentHealth + "/" + maxHealth);
+        UIHealthBar.instance.SetValue(currentHealth / (float)maxHealth);
+
     }
 
     //Funcion que se llama al lanzar un proyectil
